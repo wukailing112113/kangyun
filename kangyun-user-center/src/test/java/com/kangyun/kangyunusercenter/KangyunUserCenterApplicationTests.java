ab4222fa532ec347fd453ac898cd29a569c89839
wukl <wukl@massestech.com>
2019-12-10 13:59:51 +0800

    添加bootstrap.yml

--- kangyun-user-center/src/test/java/com/kangyun/kangyunusercenter/KangyunUserCenterApplicationTests.java
+++ kangyun-user-center/src/test/java/com/kangyun/kangyunusercenter/KangyunUserCenterApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kangyun.kangyunusercenter;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KangyunUserCenterApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.kangyun.kangyunusercenter;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KangyunUserCenterApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
